c94f065b657cae3f0293e1bfd372368eecfdb0ab
IgorBezusenko <bezusenko_98@mail.ru>
2021-09-24 12:05:54 +0300

    fix bug with focus into MoviePreview on ButtonPlay

--- src/components/Buttons/ButtonMovie.js
+++ src/components/Buttons/ButtonMovie.js
@@ -1,9 +1,17 @@
 import css from "./ButtonMovie.module.less"
 import Spottable from "@enact/spotlight/Spottable";
+import {useEffect, useRef} from "react";
 
- const MovieBtn = ({title, children, ...rest}) => {
+const MovieBtn = ({title, children,  ...rest}) => {
+    const selectPlay = useRef(null)
+    useEffect(()=>{
+        if (title==="Смотреть") {
+            selectPlay.current.focus()
+        }
+    },[])
+
     return (
-        <div  {...rest}  >
+        <div  {...rest} ref={selectPlay} >
             <button className={css.btn__movie}>{children}</button>
             <div className={css.text__movie}>{title}</div>
         </div>)
